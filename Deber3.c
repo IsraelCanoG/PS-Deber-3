e7e702081945de570d24eb67e36cc9d4231e08ad
Bosco Andrade Bravo <boscoand@hotmail.com>
2017-05-20 18:33:25 -0500

    Implementando funciones de cifrado

--- Deber3.c
+++ Deber3.c
@@ -3,19 +3,78 @@
 
 void main(){
 
-	char mensaje[1000];
-	int llave;
+	char mensaje[1000]="abcdefghijklmnopqrstuvwyyz";
+	int llave=-1,i,j,tmp;
 
-	printf("\n\n***********************************************************************************************\n\n");
-	printf("\tIngrese el mensaje a cifrar: \n");
-	scanf("%s",&mensaje);
-	printf("\tIngrese la llave numerica  : \n"); 	
-	scanf("%i",&llave);
+	printf("%s\n",mensaje);
+	minusculaMayuscula(mensaje,llave);
+	printf("%s",mensaje);
 
-	printf("\n%s",mensaje);
+//	printf("\n\n***********************************************************************************************\n\n");
+//	printf("\tIngrese el mensaje a cifrar: \n");
+//	scanf("%s",&mensaje);
+//	printf("\tIngrese la llave numerica  : \n");
+//	scanf("%i",&llave);
+//	char c = 'A';
+//	c = 'z';
+//	printf("%i",c)}/	printf("\n%s",mensaje);
 
 
 
 }
+
+
+int nuevoValor( int min, int max, int pos, int desfase){
+	int tmp;
+	if(desfase>0){
+        	if((pos+desfase)>max){
+                	tmp=pos+desfase-max;
+                        pos=(min-1)+tmp;
+                }else{
+                        pos=pos+desfase;
+                }
+         }else{
+                if((pos+desfase)<min){
+          	      	tmp=min-(pos+desfase);
+                	pos=(max+1)-tmp;
+                }else{
+         		pos=pos+desfase;
+            	}
+         }
+	return pos;
+}
+
+void minusculaMayuscula(char mensaje[], int desfase){
+
+	int i;
+
+	for(i=0;mensaje[i]!='\0';i++){
+
+		if(mensaje[i]>=65 && mensaje[i]<=90){
+			mensaje[i]=nuevoValor(65,90,mensaje[i],desfase);
+		}
+		if(mensaje[i]>=97 && mensaje[i]<=122){
+			mensaje[i]=nuevoValor(97,122,mensaje[i],desfase);
+		}
+
+	}
+
+	return;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
