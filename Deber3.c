0fef615b254df9c19a6f84df49592fe2e539534d
Bosco Andrade Bravo <boscoand@hotmail.com>
2017-05-21 05:55:53 -0500

    Culminando primera parte del Taller

--- Deber3.c
+++ Deber3.c
@@ -1,41 +1,77 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-void main(){
 
-	char mensaje[1000];
-	int llave;
+void main(int arg, char *argv[]){
 
-	printf("\n\n***********************************************************************************************\n\n");
-	printf("\tCifrado ciclico\n");
-	printf("\tIngrese mensaje a cifrar     : ");
-	scanf("%[^\n]",mensaje);
-	printf("\tIngrese la llave numerica    : ");
-	scanf("%i",&llave);
-	minusculaMayuscula(mensaje,llave);
-	printf("\tMensaje cifrado              : %s",mensaje);
-	printf("\n\n***********************************************************************************************\n\n");
+	char mensaje[1000], llave[1000];
+
+
+	if(arg==3){
+
+		strcpy(mensaje,argv[2]);
+		minusculaMayuscula(mensaje,llave);
+		printf("\n\n***********************************************************************************************\n\n");
+	        printf("\tCifrado ciclico\n");
+        	printf("\tIngrese mensaje a cifrar     : %s\n",argv[2]);
+	        printf("\tIngrese la llave numerica    : %i\n",*argv[1]);
+	        printf("\tMensaje cifrado              : %s\n",mensaje);
+		printf("\n\n***********************************************************************************************\n\n");
+
+	}
+
+	if(arg==1){
+
+		printf("\n\n***********************************************************************************************\n\n");
+		printf("\tCifrado ciclico\n");
+		printf("\tIngrese mensaje a cifrar     : ");
+		scanf("%[^\n]",mensaje);
+		printf("\tIngrese la llave numerica    : ");
+		scanf("%s",llave);
+		if(esNumero(llave)){
+			minusculaMayuscula(mensaje,atoi(llave));
+			printf("\tMensaje cifrado              : %s",mensaje);
+		}else{
+			printf("\n\tERROR, INGRESE UNA LLAME NUMERICA VALIDA");
+		}
+		printf("\n\n***********************************************************************************************\n\n");
+
+	}
 
 
 }
 
+int esNumero(char num[]){
+	int i;
+	for(i=0;num[i]!='\0';i++){
+		if(!(num[i]>=48 && num[i]<=57 || num[i]==45 || num[i]==43)){
+			return 0;
+		}
+	}
+	return 1;
+}
 
 int nuevoValor( int min, int max, int pos, int desfase){
-	int tmp;
+	int tmp,i;
 	if(desfase>0){
-        	if((pos+desfase)>max){
-                	tmp=pos+desfase-max;
-                        pos=(min-1)+tmp;
-                }else{
-                        pos=pos+desfase;
-                }
+
+		for (i=1;i<=desfase;i++){
+			if(pos<max){
+				pos=pos+1;
+			}else{
+				pos=min;
+			}
+		}
          }else{
-                if((pos+desfase)<min){
-          	      	tmp=min-(pos+desfase);
-                	pos=(max+1)-tmp;
-                }else{
-         		pos=pos+desfase;
-            	}
+                for (i=1;i<=(desfase*-1);i++){
+                        if(pos>min){
+                                pos=pos-1;
+                        }else{
+                                pos=max;
+                        }
+                }
+
          }
 	return pos;
 }
