885117895a559a9e6c51620d386eda4f0fc4afa9
Israel <israelcanog7@gmail.com>
2017-06-03 22:41:32 -0500

    Funciones

--- src/cifrado.c
+++ src/cifrado.c
@@ -69,6 +69,7 @@ char* mensajeSinEspacios(char* mensaje) {
     return mensaje_temp;
 }
 
+<<<<<<< Updated upstream
 /*
 //  @return array de espacios en blanco de mensajes y el resto de ceros
 */
@@ -85,6 +86,8 @@ int * posicionesEspacios(char * mensaje) {
     }
     return espacios;
 }
+=======
+>>>>>>> Stashed changes
 
 char * llaveEspaciada(char * llave, int * posiciones_espacios) {
     char *llaveEspaciada = (char *)malloc(sizeof(char) *50);
@@ -187,8 +190,74 @@ char* cifradoContrasena(char* mensaje, char* llave){
 }
 // fin cifrado contrasena
 
+<<<<<<< Updated upstream
+=======
+>>>>>>> Stashed changes
 
-char* cifradoAutollave(char* mensaje, char* llave){
-  char *tmp = "";
-  return tmp;
+char *cifrarAutoLlave2(char *mensaje,char *llave){ 
+	int k = 0;
+	const char *str1 = llave;
+	const char *str2 = mensaje;	
+	char *pass = malloc(strlen(str1) + strlen(str2) + 1);
+	while(*str1){
+		pass[k++]=*str1++;
+	}
+  	while(*str2){
+      		pass[k++]=*str2++;
+  	}
+  	pass[k]='\0';
+	
+	char *tmp;
+  	while((tmp=strchr(pass,' '))!=NULL){
+		strcpy(tmp,tmp+1);
+  	}
+
+	int i,j=0;
+    	for(i=0;i<strlen(mensaje);i++){		
+		if (mensaje[i]!=32){
+			if ((mensaje[i]>=65&&mensaje[i]<=90) && (pass[j]>=65&&pass[j]<=90)){
+				mensaje[i]=((mensaje[i]+pass[j])%26)+65;
+			}else if ((mensaje[i]>=97&&mensaje[i]<=122) && (pass[j]>=97&&pass[j]<=122)){			
+				mensaje[i]=(((mensaje[i]+pass[j])-64)%26)+97;
+			}else if ((mensaje[i]>=97&&mensaje[i]<=122) && (pass[j]>=65&&pass[j]<=90)){
+				mensaje[i]=(((mensaje[i]-32)+pass[j])%26)+97;
+			}else if ((mensaje[i]>=65&&mensaje[i]<=90) && (pass[j]>=97&&pass[j]<=122)){
+				mensaje[i]=((mensaje[i]+(pass[j]-32))%26)+65;
+			}
+			j=j+1;
+		}
+    	}
+    	return mensaje;
+}
+
+char *cifrarContrasena2(char *mensaje, char *llave){
+	int k = 0;
+	const char *str1 = llave;
+	const char *str2 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	printf("\n\tAlfabeto: %s\n", str2);
+	char *tmp;
+  	while((tmp=strchr(llave,' '))!=NULL){
+		strcpy(tmp,tmp+1);
+  	}
+	
+	char *pass = malloc(strlen(str1) + strlen(str2) + 1);
+	while(*str1){
+		pass[k++]=*str1++;
+	}
+  	while(*str2){
+      		pass[k++]=*str2++;
+  	}
+  	pass[k]='\0';
+	printf("\tNuevo Alfabeto: %s\n\n", pass);
+	int i;	
+	for(i=0;i<strlen(mensaje);i++){		
+		if (mensaje[i]!=32){
+			if ((mensaje[i]>=65&&mensaje[i]<=90)){
+				mensaje[i]=pass[mensaje[i]-65];
+			}else if (mensaje[i]>=97&&mensaje[i]<=122){
+				mensaje[i]=pass[mensaje[i]-97];
+			}
+		}
+    	}
+    	return mensaje;
 }
